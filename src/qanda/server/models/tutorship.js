76b5ec16fde3c8475c76641f8cd2aa9ab7f23ef5
z-e-r-0 <rido_40@hotmail.com>
2018-04-09 20:19:49 +0300

    .

---
+++ src/qanda/server/models/tutorship.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const orSchema = new Schema({
+  title: { type: 'String', required: true },
+  content: { type: 'String', required: true },
+  slug: { type: 'String', required: true },
+  cuid: { type: 'String', required: true },
+  dateAdded: { type: 'Date', default: Date.now, required: true },
+});
+
+export default mongoose.model('Tutorship', orSchema);
