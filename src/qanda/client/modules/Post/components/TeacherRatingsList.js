1f4a6b630a0f298ade929942c1d57bf250c89a69
Soleh Ruziboev <soleh_98@mail.ru>
2018-05-06 06:11:14 +0300

    Teacher Rating List modified

--- src/qanda/client/modules/Post/components/TeacherRatingsList.js
+++ src/qanda/client/modules/Post/components/TeacherRatingsList.js
@@ -8,72 +8,41 @@ export class TeacherRatingsList extends Component{
     super(props);
   }
   render(){
+    let teachers = ["Emmelio", "Cicek"];
     let myPosts = [];
-    let teacherArray = this.props.posts.filter(post => post.name === "Emmelio");
-    let gradingAverage = 0;
-    let teachingAverage = 0;
-    let workloadAverage = 0;
-    let result = {
-      grading: 0,
-      teaching: 0,
-      workload: 0,
-      slug: "-",
-      cuid: "-",
-      dateAdded: Date.now,
-      path: "-",
-      name: "-",
-    };
-    for (var i = 0; i < teacherArray.length; i++){
-      gradingAverage += teacherArray[i].grading;
-      teachingAverage += teacherArray[i].teaching;
-      workloadAverage += teacherArray[i].workload;
+    for (var k = 0; k < teachers.length; k++){
+      let teacherArray = this.props.posts.filter(post => post.name === teachers[k]);
+      let gradingAverage = 0;
+      let teachingAverage = 0;
+      let workloadAverage = 0;
+      let result = {
+        grading: 0,
+        teaching: 0,
+        workload: 0,
+        slug: "-",
+        cuid: "-",
+        dateAdded: Date.now,
+        path: "-",
+        name: "-",
+      };
+      for (var i = 0; i < teacherArray.length; i++){
+        gradingAverage += teacherArray[i].grading;
+        teachingAverage += teacherArray[i].teaching;
+        workloadAverage += teacherArray[i].workload;
+      }
+      gradingAverage /= teacherArray.length;
+      teachingAverage /= teacherArray.length;
+      workloadAverage /= teacherArray.length;
+      result.grading = gradingAverage.toFixed(1);
+      result.teaching = teachingAverage.toFixed(1);
+      result.workload = workloadAverage.toFixed(1);
+      result.name = teacherArray[0].name;
+      result.path = teacherArray[0].path;
+      result.slug = teacherArray[0].slug;
+      result.cuid = teacherArray[0].cuid;
+      result.dateAdded = teacherArray[0].dateAdded;
+      myPosts.push(result);
     }
-    gradingAverage /= teacherArray.length;
-    teachingAverage /= teacherArray.length;
-    workloadAverage /= teacherArray.length;
-    result.grading = gradingAverage.toFixed(1);
-    result.teaching = teachingAverage.toFixed(1);
-    result.workload = workloadAverage.toFixed(1);
-    result.name = teacherArray[0].name;
-    result.path = teacherArray[0].path;
-    result.slug = teacherArray[0].slug;
-    result.cuid = teacherArray[0].cuid;
-    result.dateAdded = teacherArray[0].dateAdded;
-    myPosts.push(result);
-
-    teacherArray = this.props.posts.filter(post => post.name === "Cicek");
-    gradingAverage = 0;
-    teachingAverage = 0;
-    workloadAverage = 0;
-    result = {
-      grading: 0,
-      teaching: 0,
-      workload: 0,
-      slug: "-",
-      cuid: "-",
-      dateAdded: Date.now,
-      path: "-",
-      name: "-",
-    };
-    for (var i = 0; i < teacherArray.length; i++){
-      gradingAverage += teacherArray[i].grading;
-      teachingAverage += teacherArray[i].teaching;
-      workloadAverage += teacherArray[i].workload;
-    }
-    gradingAverage /= teacherArray.length;
-    teachingAverage /= teacherArray.length;
-    workloadAverage /= teacherArray.length;
-    result.grading = gradingAverage.toFixed(1);
-    result.teaching = teachingAverage.toFixed(1);
-    result.workload = workloadAverage.toFixed(1);
-    result.name = teacherArray[0].name;
-    result.path = teacherArray[0].path;
-    result.slug = teacherArray[0].slug;
-    result.cuid = teacherArray[0].cuid;
-    result.dateAdded = teacherArray[0].dateAdded;
-    myPosts.push(result);
-
-
     return (
       <div style = {{marginTop: "20px"}}>
       <div className="card-columns">
